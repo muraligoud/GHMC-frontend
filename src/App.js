0aaa229cd6e51c67236c1e2d239cf8262d75496c
revanth892 <55280048+revanth892@users.noreply.github.com>
2023-08-02 08:23:31 +0530

    added ruoting statements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter as Router, Link, Routes, Route} from 'react-router-dom'
 // import Houseform from './components/Houseform';
 // import Driverform from './components/Driverform';
 import Adminform from './components/Adminform'
@@ -6,7 +7,15 @@ function App() {
   return (
     // <Houseform />
     // <Driverform />
-    <Adminform/>
+    // <Adminform/>
+    <Router>
+      <Routes>  
+        <Route path='/' element={}>
+        <Route path="/admin" element={}/>
+        <Route path="/driver" element={}/>
+        <Route path="/user" element={}/>
+      </Routes>
+    </Router>
   );
 }
 
